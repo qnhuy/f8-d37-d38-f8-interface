431392e518aa3a9f01b994ef331fc70594416efc
Nguyen Quang Huy <ngquanghuy.work@gmail.com>
2026-03-31 15:51:22 +0700

    fix: import image from local

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,21 +1,37 @@
 import styles from './ProductList.module.scss'
-import withLoading from "../../hoc/withLoading";
+import withLoading from '../../hoc/withLoading'
+import iphone16Plus from '../../images/iphone-16plus.jpg'
+import oppoA6Pro from '../../images/oppo-a6pro.jpg'
+import samsunGalaxyA60 from '../../images/samsung-galaxy-a06.jpg'
 
 const ProductList = withLoading(() => {
-    const products = [
-        { name: 'iPhone 17 Plus', imgUrl: 'https://iphone17.vn/wp-content/uploads/2024/11/600_16x_2_1.jpg' },
-        { name: 'Samsung S25 Ultra', imgUrl: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTBnsV4DsZCKPtTJUnph2PpWy-lf_aZczKdsaLXb6Ml07_yQGrtSRVetC1VFC6pmfidrJ1xSfO7z_jCEWTatchHjvMUGjoY5oJijTOvx-Nxi2I3adR40Wj75-KkLcc2uPGcTLqE0g&usqp=CAc' },
-        { name: 'Xiaomi 17 Pro Max', imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc736daH-NnDqPTebt9S1yQu9l55euHqcxGg&s' },
-    ];
-    return <div className={styles.productContainer}>
-        <h3 className={styles.title}>Own Products</h3>
-        <ul className={styles.productList}>
-            {products.map((product, index) => <li key={index} className={styles.productItem}>
-                <img className={styles.productImg} src={product.imgUrl} alt="" />
-                <p className={styles.productName}>{product.name}</p>
-            </li>)}
-        </ul>
+  const products = [
+    {
+      name: 'iPhone 16 Plus',
+      imgUrl: iphone16Plus,
+    },
+    {
+      name: 'Oppo A6 Pro',
+      imgUrl: oppoA6Pro,
+    },
+    {
+      name: 'Samsung Galaxy A60',
+      imgUrl: samsunGalaxyA60,
+    },
+  ]
+  return (
+    <div className={styles.productContainer}>
+      <h3 className={styles.title}>Own Products</h3>
+      <ul className={styles.productList}>
+        {products.map((product, index) => (
+          <li key={index} className={styles.productItem}>
+            <img className={styles.productImg} src={product.imgUrl} alt='' />
+            <p className={styles.productName}>{product.name}</p>
+          </li>
+        ))}
+      </ul>
     </div>
+  )
 })
 
 export default ProductList
--- src/layouts/DefaultLayout/components/AppLogo/index.jsx
+++ src/layouts/DefaultLayout/components/AppLogo/index.jsx
@@ -1,11 +1,14 @@
-import { NavLink } from "react-router"
+import { NavLink } from 'react-router'
 import styles from './AppLogo.module.scss'
+import logo from '../../../../images/app-logo.png'
 
 const AppLogo = function () {
-    return <NavLink to={'/'} className={styles.container}>
-        <img className={styles.img} src="https://zoom.fullstack.edu.vn/assets/f8_icon.png" alt="app logo" />
-        <p className={styles.slogan}>Học Lập Trình Để Đi Làm</p>
+  return (
+    <NavLink to={'/'} className={styles.container}>
+      <img className={styles.img} src={logo} alt='app logo' />
+      <p className={styles.slogan}>Học Lập Trình Để Đi Làm</p>
     </NavLink>
+  )
 }
 
 export default AppLogo
